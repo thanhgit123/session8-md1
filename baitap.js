a4a3fbb7026e056a5f84adf5c84080d33c91439c
thanhgit123 <hai880345@gmail.com>
2023-08-21 12:03:12 +0700

    add

---
+++ baitap.js
@@ -0,0 +1,20 @@
+// console.log("wellcome to Rikkei Âcdemy");
+// prompt("Mời bạn nhập số tuổi của mình");
+// document.write("Quyết tâm học lập trình javascrip");
+// alert("Chào mừng bạn đến với học viện Rikkei Âcdemy");
+// confirm("Bạn đã đủ 18 tuổi chưa");
+
+// let physic=Number(prompt("nhap diem ly"));
+// let math= Number(prompt("nhap diem toan"));
+// let chem= Number(prompt("nhap diem hoa"));
+// let diemtong= physic + math + chem;
+// let diemtrungbinh= diemtong / 3;
+// console.log("diemtong",diemtong);
+// console.log("diemtrungbinh",diemtrungbinh);
+
+
+let radius=prompt("mời nhaapoj bán kính hình tròn");
+const PI=3.14;
+let result= radius*radius*PI;
+console.log("dien tich hình tron");
+console.log("dien tich hinh tròn có ban kinh ${radius} :",result)
